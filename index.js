51d2883ea70884f13bb73604f3b242a603840f03
Lyoung0789 <Lyoung070789@gmail.com>
2020-05-14 15:14:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers)
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+function moveDodgerRight(){
+    let rightNumbers = dodger.style.left.replace("px","")
+    let right = parseInt(rightNumbers)
+
+    if (right < 360){
+        dodger.style.left = `${right + 1}px`
+    }
+
+}
