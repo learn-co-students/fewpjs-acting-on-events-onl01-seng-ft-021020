c99a130322c2d980c83e497e4532bc7f5f021335
taylorkraft <taylorkraft16@gmail.com>
2020-05-15 16:08:30 -0600

    Done.

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ function moveDodgerLeft() {
   }
 }
 
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left +1}px`;
+  }
+}
+
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
   }
 });
-
-function moveDodgerRight() {
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
-
-  if (left < 180) {
-    dodger.style.left = `${left +1}px`;
-  }
-}
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowRight") {
