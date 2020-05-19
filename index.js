a3195f8664adc41f1b285fcc92f0765f51b7c018
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-05-19 03:06:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+       document.addEventListener("keydown", function(e) {
+      console.log(e.key);
+    });
+        let dodger = document.getElementById("dodger");
+    document.addEventListener("keydown", function(e) {
+      if (e.key === "ArrowLeft") {
+        let leftNumbers = dodger.style.left.replace("px", "");
+        let left = parseInt(leftNumbers, 10);
+     
+        dodger.style.left = `${left - 1}px`;
+      }
+    });
+    
+     let dodger = document.getElementById("dodger");
+     
+     function moveDodgerLeft() {
+      let leftNumbers = dodger.style.left.replace("px", "");
+      let left = parseInt(leftNumbers, 10);
+     
+      if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+      }
+    }
+    
+    document.addEventListener("keydown", function(e) {
+      if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+      }
+    });
